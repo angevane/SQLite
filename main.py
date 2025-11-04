e7b3f655410c3839f42c6930dbc16d0e24373228
Angevane <ladutra144@gmail.com>
2025-11-04 17:35:22 -0300

    adicionando livros

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sqlite3
 
 
-executar = sqlite3.connect("livro.db")
+executar = sqlite3.connect("Livros.db")
 
 cursor = executar.cursor()
 
 cursor.execute("""
-               CREATE TABLE IF NOT EXISTS livro (
+               CREATE TABLE IF NOT EXISTS Livros (
                id INTEGER PRIMARY KEY AUTOINCREMENT, 
                titulo TEXT NOT NULL UNIQUE, 
                autor TEXT, 
@@ -15,3 +15,9 @@ cursor.execute("""
                disponivel INTEGER)
                 """)
 
+INSERT INTO Livros (titulo, autor, ano, genero, disponivel) VALUES
+('A Sombra do Vento', 'Carlos Ruiz', 'Zafón', 2001, 'Mistério', 1),
+('1984', 'George Orwell',1949, 'Ficção Científica', 0)
+('O Senhor dos Anéis', 'J.R.R. Tolkien',1954, 'Fantasia', 1)
+('Dom Quixote', 'Miguel de Cervantes',1605, 'Romance', 1)
+('Cem Anos de Solidão', 'Gabriel García Márquez',1967, 'Realismo Mágico', 0);

a5a82f585889d50930cf2c33352c2a9bafe12287
Angevane <ladutra144@gmail.com>
2025-11-04 16:58:30 -0300

    atualizando a tabela livros

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ executar = sqlite3.connect("livro.db")
 cursor = executar.cursor()
 
 cursor.execute("""
-               CREATE TABLE IF NOT EXISTS livro (id INTEGER, titulo TEXT, 
-               autor TEXT, ano INTEGER, genero TEXT, disponivel INTEGER)
-""")
+               CREATE TABLE IF NOT EXISTS livro (
+               id INTEGER PRIMARY KEY AUTOINCREMENT, 
+               titulo TEXT NOT NULL UNIQUE, 
+               autor TEXT, 
+               ano INTEGER,
+               genero TEXT, 
+               disponivel INTEGER)
+                """)
+
